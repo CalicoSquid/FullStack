7cb4c8271a647a159db9a6c75d3df8a52b9cdedb
Caleb Mow <immensedrama@gmail.com>
2023-12-16 17:36:41 +0100

    Exercises 1.1.-1.2.

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,60 +1,49 @@
-import logo from "./assets/react.svg"
+import logo from "./assets/react.svg";
 
-const Header = (props) => {
-  return <h1>{props.course}</h1>;
+const Header = ({ course }) => {
+  return <h1>{course.name}</h1>;
 };
 
-const Part = (props) => {
-  return (
-    <p>{props.part} {props.exercise}</p>
-  )
-}
-
-const Content = (props) => {
-  return (
-    <>
-      <Part part={props.part1} exercise={props.exercises1}/>
-      <Part part={props.part2} exercise={props.exercises2}/>
-      <Part part={props.part3} exercise={props.exercises3}/>
-    </>
-  );
+const Part = ({ part }) => {
+  return <p>{part.name} {part.exercises}</p>
 };
 
-const Total = (props) => {
-  return (
-    <p>
-      Number of exercises{" "}
-      {props.exercises1 + props.exercises2 + props.exercises3}
-    </p>
-  );
+const Content = ({ course }) => {
+  const contents = course.parts.map((part) => <Part part={part} />);
+  return <>{contents}</>;
+};
+
+const Total = ({ course }) => {
+  let total = 0;
+  course.parts.forEach((num) => (total += num.exercises));
+  return <p>Number of exercises {total}</p>;
 };
 
 const App = () => {
-  const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div className="container">
-      <img src={logo} className="logo"/>
+      <img src={logo} className="logo" />
       <Header course={course} />
-      <Content
-        part1={part1}
-        part2={part2}
-        part3={part3}
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
-      />
-      <Total
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
-      />
+      <Content course={course} />
+      <Total course={course} />
     </div>
   );
 };
