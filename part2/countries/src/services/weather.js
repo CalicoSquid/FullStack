cc8a77742f93cb84130cc30ee67df0529b8bb64e
Caleb Mow <immensedrama@gmail.com>
2023-12-26 22:30:23 +0100

    Minor changes

--- part2/countries/src/services/weather.js
+++ part2/countries/src/services/weather.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_KEY = "44e4ac5b8dc0921b136dc956a50f102a";
+const API_KEY = import.meta.env.VITE_SOME_KEY;
 
 const getWeatherData = async (lat, lon) => {
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
